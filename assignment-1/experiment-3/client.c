3208a9857003cfe5c718714b17db869d96cccc05
marciorasf <marciorasf@gmail.com>
2020-09-08 18:21:27 -0300

    add experiment 3 files

---
+++ assignment-1/experiment-3/client.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#include <stdlib.h> // function exit is declared on this lib
+#include <unistd.h> // function close is declared on this lib
+#include <string.h> // function strlen is declared on this lib
+
+#define SERVER_PORT 54321
+#define MAX_LINE 256
+
+int main(int argc, char *argv[]) {
+    struct sockaddr_in sin;
+    char *host;
+    char buf[MAX_LINE];
+    int s;
+    int len;
+
+    if (argc == 2) {
+        host = argv[1];
+    }
+    else {
+        fprintf(stderr, "usage: simplex-talk host\n");
+        exit(1);
+    }
+
+    /* parse dotted IP to in_addr_t format and assign to sin.sin_addr */
+    inet_aton(host, &sin.sin_addr);
+
+    /* build address data structure */
+    bzero((char *)&sin, sizeof(sin));
+    
+    sin.sin_family = AF_INET;
+    
+    sin.sin_port = htons(SERVER_PORT);
+
+    /* active open */
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("simplex-talk: socket");
+        exit(1);
+    }
+
+    if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+        perror("simplex-talk: connect");
+        close(s);
+        exit(1);
+    }
+
+    /* main loop: get and send lines of text */
+    while (fgets(buf, sizeof(buf), stdin)) {
+        buf[MAX_LINE - 1] = '\0';
+        len = strlen(buf) + 1;
+        send(s, buf, len, 0);
+
+        // Wait for the server to send the message back and print it
+        char recvBuf[MAX_LINE];
+        recv(s, recvBuf, sizeof(recvBuf), 0);
+        fputs(recvBuf, stdout);
+    }
+}
