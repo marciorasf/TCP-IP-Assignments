9aa24932966b8050a225ba792edf6d600d6af2e5
marciorasf <marciorasf@gmail.com>
2020-09-08 12:29:29 -0300

    add experiments 1 and 2 folders and files

---
+++ assignment-1/setup/run_client.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+IP=10.0.0.8
+PORT=54321
+
+./client.out "$IP:PORT"
---
+++ assignment-1/setup/server.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include <stdlib.h> // function exit is declared on this lib
+#include <unistd.h> // function close is declared on this lib
+#include <string.h> // function strlen is declared on this lib
+
+#define SERVER_PORT 54321
+#define MAX_PENDING 5
+#define MAX_LINE 256
+
+int main() {
+    struct sockaddr_in sin;
+    char buf[MAX_LINE];
+    int s, new_s;
+
+    /* build address data structure */
+    bzero((char *)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(SERVER_PORT);
+
+    /* setup passive open */
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("simplex-talk: socket");
+        exit(1);
+    }
+
+    if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+        perror("simplex-talk: bind");
+        exit(1);
+    }
+
+    listen(s, MAX_PENDING);
+
+    unsigned int len =  sizeof(sin);
+
+    /* wait for connection, then receive and print text */
+    while (1) {
+        if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0) {
+            perror("simplex-talk: accept");
+            exit(1);
+        }
+
+        while ((len = (unsigned int) recv(new_s, buf, sizeof(buf), 0))) {
+            fputs(buf, stdout);
+        }
+        
+        close(new_s);
+    }
+}
