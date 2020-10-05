3029e8418fcba5a936034b327e74d51de765b0bd
marciorasf <marciorasf@gmail.com>
2020-10-05 20:36:35 -0300

    assignment-2: fix script for the first exercise

--- assignment-2/experiment-1.sh
+++ assignment-2/experiment-1.sh
@@ -2,13 +2,18 @@
 
 time=4
 destinations=("www.ufrj.br" "www.ucla.edu" "www.phil.cam.uk" "www.adelaide.edu.au");
-testsPerDestination=2
+testsPerDestination=10
 
 for dest in "${destinations[@]}"; do
     filename=time-$time-dest-$dest.txt
     > $filename
 
-    for _ in {1..$testsPerDestination}; do
+    for counter in $(seq 1 $testsPerDestination); do
+        echo "Iteration $counter" >> $filename;
+        echo "" >> $filename;
         traceroute $dest -4 >> $filename;
+        echo "" >> $filename;
+        echo "-----------------------------------------------------------------------" >> $filename;
+        echo "" >> $filename;
     done;
 done;
